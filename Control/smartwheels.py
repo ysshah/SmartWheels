2a88d84cce3c92f33e31c3e89f0313c9b6ded503
Yash Shah <yashshah0127@gmail.com>
2016-12-15 02:39:46 -0800

    Improve obstacle avoidance and clean code

--- Control/smartwheels.py
+++ Control/smartwheels.py
@@ -1,7 +1,3 @@
-import sys
-import threading
-import termios
-import tty
 import select
 import time
 import RPi.GPIO as GPIO
@@ -10,20 +6,12 @@ from multiprocessing import Process, Value
 from can2RNET import *
 
 
-def induceJSMerror(canSocket):
-    for i in range(3):
-        cansend(canSocket,'0c000000#')
-
-
-def RNET_JSMerror_exploit(canSocket):
-    print('Waiting for JSM heartbeat')
-    canwait(canSocket, '03C30F0F:1FFFFFFF')
-    print('Waiting for joy frame')
-    joy_id = getRNETjoystickFrameID(canSocket)
-    print('Using joy frame: ' + joy_id)
-    induceJSMerror(canSocket)
-    print('3 x 0c000000# sent')
-    return joy_id
+def setSpeedRange(canSocket, speed_range):
+    """Set speed_range from 0% - 100%."""
+    if 0 <= speed_range and speed_range <= 100:
+        cansend(canSocket,'0a040100#{:02x}'.format(speed_range))
+    else:
+        print('Invalid RNET SpeedRange: {}'.format(speed_range))
 
 
 def getRNETjoystickFrameID(canSocket):
@@ -39,12 +27,9 @@ def getRNETjoystickFrameID(canSocket):
     raise TimeoutError('No RNET-Joystick frame seen')
 
 
-def setSpeedRange(canSocket, speed_range):
-    """Set speed_range from 0% - 100%."""
-    if 0 <= speed_range and speed_range <= 100:
-        cansend(canSocket,'0a040100#{:02x}'.format(speed_range))
-    else:
-        print('Invalid RNET SpeedRange: {}'.format(speed_range))
+def induceJSMerror(canSocket):
+    for i in range(3):
+        cansend(canSocket,'0c000000#')
 
 
 def sendJoystickValuesJSMerror(x, y):
@@ -74,7 +59,7 @@ def initializeUltrasonicSensors(gpio_in, gpio_out):
     GPIO.setup(gpio_out, GPIO.OUT)
     GPIO.setup(gpio_in, GPIO.IN)
     GPIO.output(gpio_out, False)
-    print("Waiting For Sensor To Settle")
+    print('Waiting For Sensor To Settle')
     time.sleep(1)
 
 
@@ -102,13 +87,13 @@ def updateObstacles(obstacleBooleans):
     for echo, trig in echoTrigPairs:
         initializeUltrasonicSensors(echo, trig)
 
-    sensorValues = [[101] * bufferSize for i in range(numSensors)]
+    sensorValues = [[100] * bufferSize for i in range(numSensors)]
     while True:
         for i in range(numSensors):
             sensorValues[i].pop(0)
             sensorValues[i].append(getDistance(*echoTrigPairs[i]))
             average = sum(sensorValues[i]) / bufferSize
-            obstacleBooleans[i].value = (average < 80)
+            obstacleBooleans[i].value = (average < 60)
 
 
 def turnLeft(x, y):
@@ -117,7 +102,7 @@ def turnLeft(x, y):
     y.value = 0
     time.sleep(2)
     x.value = 0
-    y.value = 100
+    y.value = 80
     time.sleep(2)
     x.value = 100
     y.value = 0
@@ -130,7 +115,7 @@ def turnRight(x, y):
     y.value = 0
     time.sleep(2)
     x.value = 0
-    y.value = 100
+    y.value = 80
     time.sleep(2)
     x.value = 255 - 100
     y.value = 0
@@ -149,8 +134,6 @@ def setJoysticksFromApriltag(x, y, obstacleBooleans):
         obstacleLeft = obstacleBooleans[0].value
         obstacleCenter = obstacleBooleans[1].value
         obstacleRight = obstacleBooleans[2].value
-        # print('Obstacle Left: {}  |  Obstacle Center: {}  |  Obstacle Right: {}'.format(
-        #     obstacleLeft, obstacleCenter, obstacleRight))
 
         # Obstacle left => turn right
         if obstacleLeft and not obstacleRight:
@@ -178,7 +161,7 @@ def setJoysticksFromApriltag(x, y, obstacleBooleans):
             obstacleRight = obstacleBooleans[2].value
             while obstacleLeft and obstacleRight:
                 print('Obstacles on both sides... in loop')
-                y.value = 255 - 100
+                y.value = 255 - 80
                 time.sleep(1)
                 obstacleLeft = obstacleBooleans[0].value
                 obstacleRight = obstacleBooleans[2].value
@@ -191,89 +174,53 @@ def setJoysticksFromApriltag(x, y, obstacleBooleans):
         else:
             data, addr = sock.recvfrom(1024)
             if len(data) == 24:
-                # print('No tags')
                 x.value = 0
                 y.value = 0
             elif len(data) == 112:
                 d = struct.unpack('!8i20f', data)
 
-                # print('Corners: ({:4.0f},{:4.0f}); ({:4.0f},{:4.0f}); ({:4.0f},{:4.0f}); ({:4.0f},{:4.0f})'.format(*d[11:19]))
                 width = max(max(abs(d[11] - d[13]), abs(d[13] - d[15])),
                             max(abs(d[15] - d[17]), abs(d[17] - d[11])))
                 height = max(max(abs(d[12] - d[14]), abs(d[14] - d[16])),
                             max(abs(d[16] - d[18]), abs(d[18] - d[12])))
                 size = max(width, height)
-                # print('X: {:4.0f}, Y: {:4.0f}, SIZE: {:4.0f}'.format(d[9], d[10], size))
-                # print('Calculated distance: {:3.2f} m; {:3.2f} ft'.format(
-                #     285 / size, 3.28084 * 285 / size))
+                distanceToAprilTag = 285 / size  # Roughly meters
 
-                if size < 300:
-                    # print('going forward')
+                if distanceToAprilTag > 1.0:
                     y.value = 50
                     x_val = d[9]
                     if x_val >= x_center:
                         x.value = int((x_val - x_center) * (80 / x_scale))
                     else:
                         x.value = 255 - int((x_center - x_val) * (80 / x_scale))
-                elif size < 400:
-                    # print('stopping')
+                elif distanceToAprilTag > 0.85:
                     x.value = 0
                     y.value = 0
                 else:
-                    # print('going backwards')
                     y.value = 255 - 50
 
 
 if __name__ == "__main__":
 
-    # canSocket = opencansocket(0)
-    # setSpeedRange(canSocket, 0)
-    # canwait(canSocket, '03C30F0F:1FFFFFFF')
-    # joy_id = getRNETjoystickFrameID(canSocket)
-    # induceJSMerror(canSocket)
-    # mintime = .01
-    # nexttime = time.time() + mintime
-
-    # n_count = 220
-
-    # count = 0
-    # while count < n_count:
-    #     cansend(canSocket, '{}#{:02x}{:02x}'.format(joy_id, 100, 0))
-    #     time.sleep(.01)
-    #     count += 1
-
-    # count = 0
-    # while count < n_count:
-    #     cansend(canSocket, '{}#{:02x}{:02x}'.format(joy_id, 0, 0))
-    #     time.sleep(.01)
-    #     count += 1
-
-    # count = 0
-    # while count < 235:
-    #     cansend(canSocket, '{}#{:02x}{:02x}'.format(joy_id, 155, 0))
-    #     time.sleep(.01)
-    #     count += 1
-        # nexttime += mintime
-        # t = time.time()
-        # if t < nexttime:
-        #     time.sleep(nexttime - t)
-        # else:
-        #     nexttime += mintime
-
     x = Value('i', 0)
     y = Value('i', 0)
-    obstacleBooleans = (Value('b', False), Value('b', False), Value('b', False))
+    obstacleBooleans = (
+        Value('b', False), Value('b', False), Value('b', False)
+    )
 
-    # p1 = Process(target=changeValuesByTime, args=(x, y, obstaclePresent))
-    p1 = Process(target=setJoysticksFromApriltag,
+    obstacleProcess = Process(target=updateObstacles,
+        args=(obstacleBooleans,))
+    obstacleProcess.start()
+    time.sleep(3)
+
+    sendJoystickValuesProcess = Process(target=sendJoystickValuesJSMerror,
+        args=(x, y))
+    sendJoystickValuesProcess.start()
+
+    setJoystickProcess = Process(target=setJoysticksFromApriltag,
         args=(x, y, obstacleBooleans))
-    p2 = Process(target=sendJoystickValuesJSMerror, args=(x, y))
-    p3 = Process(target=updateObstacles, args=(obstacleBooleans,))
+    setJoystickProcess.start()
 
-    p1.start()
-    p2.start()
-    p3.start()
-
-    p1.join()
-    p2.join()
-    p3.join()
+    obstacleProcess.join()
+    sendJoystickValuesProcess.join()
+    setJoystickProcess.join()
