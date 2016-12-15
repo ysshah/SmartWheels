2268329e72542851e49a65cd24bb30f280a7cc6b
Yash Shah <yashshah0127@gmail.com>
2016-12-15 00:12:36 -0800

    Change threading to multiprocessing

--- Control/smartwheels.py
+++ Control/smartwheels.py
@@ -5,109 +5,127 @@ import tty
 import select
 import time
 import RPi.GPIO as GPIO
+from multiprocessing import Process, Value
 
 from can2RNET import *
 
-TRIG1 = 27 #output
-ECHO1 = 17 #input
-TRIG2 = 6  #output
-ECHO2 = 5 #input
-TRIG3 = 19 #output
-ECHO3 = 13 #input
+
+def induceJSMerror(canSocket):
+    for i in range(3):
+        cansend(canSocket,'0c000000#')
 
 
-def getRNETjoystickFrameID(can_socket):
-    """
-    Returns: JoyFrame extendedID as text
-    TODO: Fix blocking read in recvfrom
-    """
-    ready = select.select([can_socket], [], [], 1.0)
+def RNET_JSMerror_exploit(canSocket):
+    print('Waiting for JSM heartbeat')
+    canwait(canSocket, '03C30F0F:1FFFFFFF')
+    print('Waiting for joy frame')
+    joy_id = getRNETjoystickFrameID(canSocket)
+    print('Using joy frame: ' + joy_id)
+    induceJSMerror(canSocket)
+    print('3 x 0c000000# sent')
+    return joy_id
+
+
+def getRNETjoystickFrameID(canSocket):
+    """Get JoyFrame extendedID as text."""
+    ready = select.select([canSocket], [], [], 1.0)
     if ready[0]:
         start = time.time()
         while (time.time() - start) < 1:
-            cf, addr = can_socket.recvfrom(16)
+            cf, addr = canSocket.recvfrom(16)
             frameid = dissect_frame(cf).split('#')[0]
             if frameid[:3] == '020':
                 return frameid
     raise TimeoutError('No RNET-Joystick frame seen')
 
 
-def setSpeedRange(cansocket,speed_range):
+def setSpeedRange(canSocket, speed_range):
     """Set speed_range from 0% - 100%."""
     if 0 <= speed_range and speed_range <= 100:
-        cansend(cansocket,'0a040100#{:02x}'.format(speed_range))
+        cansend(canSocket,'0a040100#{:02x}'.format(speed_range))
     else:
         print('Invalid RNET SpeedRange: {}'.format(speed_range))
 
 
-def inject_rnet_joystick_frame(can_socket, rnet_joystick_id):
-    """Wait for joyframe and inject another spoofed frame ASAP."""
-    # Prebuild the frame we are waiting on
+def sendJoystickValuesJSMerror(x, y):
+    canSocket = opencansocket(0)
+    setSpeedRange(canSocket, 0)
+    canwait(canSocket, '03C30F0F:1FFFFFFF')
+    joystickID = getRNETjoystickFrameID(canSocket)
+    induceJSMerror(canSocket)
+    while True:
+        cansend(canSocket, '{}#{:02x}{:02x}'.format(joystickID, x.value, y.value))
+        time.sleep(.01)
+
+
+def sendJoystickValues(x, y):
+    canSocket = opencansocket(0)
+    setSpeedRange(canSocket, 0)
+    rnet_joystick_id = getRNETjoystickFrameID(canSocket)
     rnet_joystick_frame_raw = build_frame(rnet_joystick_id + '#0000')
-    while rnet_threads_running:
-        cf, addr = can_socket.recvfrom(16)
+    while True:
+        cf, addr = canSocket.recvfrom(16)
         if cf == rnet_joystick_frame_raw:
-            cansend(can_socket, '{}#{:02x}{:02x}'.format(
-                rnet_joystick_id, joystick_x, joystick_y))
+            cansend(canSocket, '{}#{:02x}{:02x}'.format(
+                rnet_joystick_id, x.value, y.value))
 
 
 def initializeUltrasonicSensors(gpio_in, gpio_out):
-    GPIO.setup(gpio_out,GPIO.OUT)
-    GPIO.setup(gpio_in,GPIO.IN)
+    GPIO.setup(gpio_out, GPIO.OUT)
+    GPIO.setup(gpio_in, GPIO.IN)
     GPIO.output(gpio_out, False)
     print("Waiting For Sensor To Settle")
     time.sleep(1)
 
 
 def getDistance(echo, trig):
-    global distance1
-    global distance2
-    global distance3
     GPIO.output(trig, True)
     time.sleep(0.00001)
     GPIO.output(trig, False)
-    while GPIO.input(echo)==0:
+    while GPIO.input(echo) == 0:
         pulse_start = time.time()
 
-    while GPIO.input(echo)==1:
+    while GPIO.input(echo) == 1:
         pulse_end = time.time()
 
     pulse_duration = pulse_end - pulse_start
     distance = pulse_duration * 17150
+    return distance
 
-    distance = round(distance, 2)
-    if (echo == ECHO1):
-        distance1 = round(distance, 2)
-        #print("Distance1: " + str(distance1))
-    elif (echo == ECHO2):
-        distance2 = round(distance, 2)
-        #print("Distance2: " + str(distance2))
-    elif (echo == ECHO3):
-        distance3 = round(distance, 2)
 
-    #print("Sensor" + str(ultrasound) + "Distance: " +  ":" + str(distance) + " cm",)
-    time.sleep(0.1)
+def updateObstacles(obstaclePresent):
+    bufferSize = 10
+    numSensors = 3
 
-def getDistances():
+    GPIO.setmode(GPIO.BCM)
+    echoTrigPairs = [(17, 27), (5, 6), (13, 19)]
+    for echo, trig in echoTrigPairs:
+        initializeUltrasonicSensors(echo, trig)
+
+    sensorValues = [[101] * bufferSize for i in range(numSensors)]
     while True:
-        getDistance(ECHO1, TRIG1)
-        getDistance(ECHO2, TRIG2)
-        getDistance(ECHO3, TRIG3)
-        print("Distance1: " + str(distance1))
-        print("Distance2: " + str(distance2))
-        print("Distance3: " + str(distance3))
-        time.sleep(0.5)
+        obstacles = False
+        for i in range(numSensors):
+            sensorValues[i].pop(0)
+            sensorValues[i].append(getDistance(*echoTrigPairs[i]))
+            average = sum(sensorValues[i]) / bufferSize
+            if average < 80:
+                obstacles = True
+                break
+        obstaclePresent.value = obstacles
 
-def stopWheelchair():
-   global joystick_x
-   global joystick_y
-   joystick_x = 0
-   joystick_y = 0
 
-def control():
-    global joystick_x
-    global joystick_y
+def changeValuesByTime(x, y, obstaclePresent):
+    while True:
+        if obstaclePresent.value:
+            y.value = 0
+        else:
+            y.value = 50
+    # y.value = 155
+    # time.sleep(3)
 
+
+def setJoysticksFromApriltag(x, y, obstaclePresent):
     resolution = (1920, 1080)
     x_center = resolution[0] / 2
     x_scale = resolution[1] / 2
@@ -116,133 +134,94 @@ def control():
     # Raspberry Pi IP address, AprilTags UDP port
     sock.bind(('172.20.10.5', 7709))
     while True:
-        data, addr = sock.recvfrom(1024)
-        if len(data) == 24:
-            print('No tags')
-            stopWheelchair()
-        elif len(data) == 112:
-            d = struct.unpack('!8i20f', data)
+        if obstaclePresent.value:
+            x.value = 0
+            y.value = 0
+        else:
+            data, addr = sock.recvfrom(1024)
+            if len(data) == 24:
+                print('No tags')
+                x.value = 0
+                y.value = 0
+            elif len(data) == 112:
+                d = struct.unpack('!8i20f', data)
 
-            print('Corners: ({:4.0f},{:4.0f}); ({:4.0f},{:4.0f}); ({:4.0f},{:4.0f}); ({:4.0f},{:4.0f})'.format(*d[11:19]))
-            width = max(max(abs(d[11] - d[13]), abs(d[13] - d[15])),
-                        max(abs(d[15] - d[17]), abs(d[17] - d[11])))
-            height = max(max(abs(d[12] - d[14]), abs(d[14] - d[16])),
-                        max(abs(d[16] - d[18]), abs(d[18] - d[12])))
-            size = max(width, height)
-            print('X: {:4.0f}, Y: {:4.0f}, SIZE: {:4.0f}'.format(d[9], d[10], size))
-            print('Calculated distance: {:3.2f} m; {:3.2f} ft'.format(
-                285 / size, 3.28084 * 285 / size))
+                print('Corners: ({:4.0f},{:4.0f}); ({:4.0f},{:4.0f}); ({:4.0f},{:4.0f}); ({:4.0f},{:4.0f})'.format(*d[11:19]))
+                width = max(max(abs(d[11] - d[13]), abs(d[13] - d[15])),
+                            max(abs(d[15] - d[17]), abs(d[17] - d[11])))
+                height = max(max(abs(d[12] - d[14]), abs(d[14] - d[16])),
+                            max(abs(d[16] - d[18]), abs(d[18] - d[12])))
+                size = max(width, height)
+                print('X: {:4.0f}, Y: {:4.0f}, SIZE: {:4.0f}'.format(d[9], d[10], size))
+                print('Calculated distance: {:3.2f} m; {:3.2f} ft'.format(
+                    285 / size, 3.28084 * 285 / size))
 
-            if size < 300:
-                print('going forward')
-                joystick_y = 50
-                x = d[9]
-                if x >= x_center:
-                    joystick_x = int((x - x_center) * (80 / x_scale))
+                if size < 300:
+                    print('going forward')
+                    y.value = 50
+                    x_val = d[9]
+                    if x_val >= x_center:
+                        x.value = int((x_val - x_center) * (80 / x_scale))
+                    else:
+                        x.value = 255 - int((x_center - x_val) * (80 / x_scale))
+                elif size < 400:
+                    print('stopping')
+                    x.value = 0
+                    y.value = 0
                 else:
-                    joystick_x = 255 - int((x_center - x) * (80 / x_scale))
-            elif size < 400:
-                print('stopping')
-                stopWheelchair()
-            else:
-                print('going backwards')
-                joystick_y = 255 - 50
-
-
-def manualOverride():
-    global joystick_x
-    global joystick_y
-    global rnet_threads_running
-
-    def getch():
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-
-    while True:
-#        getDistances()
-        char = getch()
-        if char == 'w':
-            joystick_y = 127
-        elif char == 'd':
-            joystick_x = 127
-        elif char == 'a':
-            joystick_x = 255 - 127
-        elif char == 's':
-            joystick_y = 255 - 127
-        elif char == 'x':
-            joystick_x = 0
-            joystick_y = 0
-            rnet_threads_running = False
-            break
-        char = ''
-        time.sleep(0.2)
-        joystick_x = 0
-        joystick_y = 0
+                    print('going backwards')
+                    y.value = 255 - 50
 
 
 if __name__ == "__main__":
-    global joystick_x
-    global joystick_y
-    global distance1
-    global distance2
-    global distance3
-    global rnet_threads_running
-    joystick_x = 0
-    joystick_y = 0
-    distance1 = 0
-    distance2 = 0
-    distance3 = 0
-    rnet_threads_running = True
 
-    GPIO.setmode(GPIO.BCM)
-    initializeUltrasonicSensors(ECHO1, TRIG1)
-    initializeUltrasonicSensors(ECHO2, TRIG2)
-    initializeUltrasonicSensors(ECHO3, TRIG3)
+    # canSocket = opencansocket(0)
+    # setSpeedRange(canSocket, 0)
+    # canwait(canSocket, '03C30F0F:1FFFFFFF')
+    # joy_id = getRNETjoystickFrameID(canSocket)
+    # induceJSMerror(canSocket)
+    # mintime = .01
+    # nexttime = time.time() + mintime
 
-    can_socket = opencansocket(0)
+    # n_count = 220
 
-    # RNET joystick connection
-    rnet_joystick_id = getRNETjoystickFrameID(can_socket)
+    # count = 0
+    # while count < n_count:
+    #     cansend(canSocket, '{}#{:02x}{:02x}'.format(joy_id, 100, 0))
+    #     time.sleep(.01)
+    #     count += 1
 
-    # Set chair's speed to the lowest setting
-    setSpeedRange(can_socket, 0)
+    # count = 0
+    # while count < n_count:
+    #     cansend(canSocket, '{}#{:02x}{:02x}'.format(joy_id, 0, 0))
+    #     time.sleep(.01)
+    #     count += 1
 
-    # if len(sys.argv) == 4:
-    #     rnet_joystick_frame_raw = build_frame(rnet_joystick_id + "#0000")
-    #     numSent = 0
-    #     count = int(sys.argv[3])
-    #     while numSent < count:
-    #         cf, addr = can_socket.recvfrom(16)
-    #         if cf == rnet_joystick_frame_raw:
-    #             numSent += 1
-    #             # print('sending on index {}'.format(i)))
-    #             cansend(can_socket, '{}#{:02x}{:02x}'.format(
-    #                 rnet_joystick_id, int(sys.argv[1]), int(sys.argv[2])))
-    #     print('sent {} times'.format(count))
+    # count = 0
+    # while count < 235:
+    #     cansend(canSocket, '{}#{:02x}{:02x}'.format(joy_id, 155, 0))
+    #     time.sleep(.01)
+    #     count += 1
+        # nexttime += mintime
+        # t = time.time()
+        # if t < nexttime:
+        #     time.sleep(nexttime - t)
+        # else:
+        #     nexttime += mintime
 
-    inject_rnet_joystick_frame_thread = threading.Thread(
-        target=inject_rnet_joystick_frame,
-        args=(can_socket, rnet_joystick_id,),
-        daemon=True)
-    inject_rnet_joystick_frame_thread.start()
+    x = Value('i', 0)
+    y = Value('i', 0)
+    obstaclePresent = Value('b', False)
 
-    if len(sys.argv) == 1 or sys.argv[1] != '-m':
-        print('Autonomous control mode enabled')
-        control_thread = threading.Thread(target=control, daemon=True)
-    else:
-        print('Manual override mode enabled')
-        control_thread = threading.Thread(target=manualOverride, daemon=True)
-        sensors_thread = threading.Thread(target=getDistances, daemon=True)
-        
+    # p1 = Process(target=changeValuesByTime, args=(x, y, obstaclePresent))
+    p1 = Process(target=setJoysticksFromApriltag, args=(x, y, obstaclePresent))
+    p2 = Process(target=sendJoystickValuesJSMerror, args=(x, y))
+    p3 = Process(target=updateObstacles, args=(obstaclePresent,))
 
-    control_thread.start()
-    sensors_thread.start()
+    p1.start()
+    p2.start()
+    p3.start()
 
-    while rnet_threads_running:
-        time.sleep(0.5)
+    p1.join()
+    p2.join()
+    p3.join()
